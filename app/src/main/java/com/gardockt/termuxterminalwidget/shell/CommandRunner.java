3a39de16a9d23d76dcc0b6c8f60f9e5ff81eaed5
gardockt <tgardocki1@gmail.com>
2024-05-11 21:14:39 +0200

    Pass command to shell stdin instead of argument

--- app/src/main/java/com/gardockt/termuxterminalwidget/shell/CommandRunner.java
+++ app/src/main/java/com/gardockt/termuxterminalwidget/shell/CommandRunner.java
@@ -39,7 +39,7 @@ public class CommandRunner {
         intent.setClassName(TermuxConstants.TERMUX_PACKAGE_NAME, TermuxConstants.TERMUX_APP.RUN_COMMAND_SERVICE_NAME);
         intent.setAction(TermuxConstants.TERMUX_APP.RUN_COMMAND_SERVICE.ACTION_RUN_COMMAND);
         intent.putExtra(TermuxConstants.TERMUX_APP.RUN_COMMAND_SERVICE.EXTRA_COMMAND_PATH, TermuxConstants.TERMUX_FILES_DIR_PATH + "/usr/bin/sh");
-        intent.putExtra(TermuxConstants.TERMUX_APP.RUN_COMMAND_SERVICE.EXTRA_ARGUMENTS, new String[]{"-c", command});
+        intent.putExtra(TermuxConstants.TERMUX_APP.RUN_COMMAND_SERVICE.EXTRA_STDIN, command);
         intent.putExtra(TermuxConstants.TERMUX_APP.RUN_COMMAND_SERVICE.EXTRA_BACKGROUND, true);
         intent.putExtra(TermuxConstants.TERMUX_APP.RUN_COMMAND_SERVICE.EXTRA_PENDING_INTENT, pendingIntent);
 
